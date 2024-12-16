37158648638dee75636ffab6f28173c0fc8fa6b4
donkey <william22850872@gmail.com>
2024-12-16 20:22:39 +0800

    'ff.f2'

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/ff/f1',function(){
     return view('ff.f1');
 });
 
+Route::get('/ff/f2',function(){
+    return view('ff.f2');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
