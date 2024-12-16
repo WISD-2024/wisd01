f362c520e4f1985833e019b0c8547aea8797f816
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-12-16 19:56:41 +0800

    cc.c1

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,10 @@ Route::get('/bb/b2', function () {
     return view('bb.b2');
 });
 
+Route::get('/cc/c1', function () {
+    return view('cc.c1');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
