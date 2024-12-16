3c669f72dfea0a9f1c46ede72d31cfc18cdafcbd
王翊安 <aud986587@gmail.com>
2024-12-16 19:31:00 +0800

    bb.b2

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/bb/b1', function () {
     return view('bb.b1');
 });
 
+Route::get('/bb/b2', function () {
+    return view('bb.b2');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
